e16957107f6008d9df37de1c12bdbfb41873606d
tangyang <tangyang@zhihu.com>
2021-03-30 13:17:33 +0800

    fix list problem

--- pilot/pkg/dns/store/egress_test.go
+++ pilot/pkg/dns/store/egress_test.go
@@ -1,7 +1,9 @@
 package store
 
 import (
+	networking "istio.io/api/networking/v1alpha3"
 	"istio.io/istio/pilot/pkg/dns/schemas"
+	"strings"
 	"testing"
 )
 
@@ -12,6 +14,19 @@ func TestEgressStore(t *testing.T) {
 	AppendToEgressScope(map[string]string{
 		"app": "reviews",
 	}, "default", "pages.default.svc.cluster.local.")
-	t.Log(EgressStore().List(schemas.EgressSidecarGVK, ""))
-
+	cfg, _ := EgressStore().List(schemas.EgressSidecarGVK, "")
+	for i, host := range cfg[0].Spec.(*networking.Sidecar).Egress[0].Hosts {
+		host = strings.TrimPrefix(host, "*/")
+		host += "."
+		switch i {
+		case 0:
+			if host != "ratings.default.svc.cluster.local." {
+				t.Fail()
+			}
+		case 1:
+			if host != "pages.default.svc.cluster.local." {
+				t.Fail()
+			}
+		}
+	}
 }
